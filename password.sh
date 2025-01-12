cb86e555ad61954f82025643989b0b97a71cf76e
Lewis Cox <lcox101101@gmail.com>
2025-01-12 17:59:41 +0000

    password generator script - checking validity of length user input

---
+++ password.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# while true; do
+	echo "length of new password:"
+	
+	read length
+
+	# check input validity
+	if [[ "$length" =~ ^[0-9]*$ ]]; then
+		if [ "$length" -lt 12 ] || [ "$length" -gt 20 ]; then
+			echo "length must be between 12 and 20 characters"
+		else
+			echo "valid length - continuing"
+		fi
+	else
+		echo "invalid input"
+	fi
+# done
