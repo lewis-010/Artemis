ed28bd9eb41a25c53e6a29d753408f3b7c7332fe
Lewis Cox <lcox101101@gmail.com>
2025-01-14 20:56:35 +0000

    create password with /dev/urandom

--- password.sh
+++ password.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# input length of new password
 while true; do
 	echo "length of new password:"
 	
 	read length
 
-	# check input validity
+	# validate input
 	if [[ "$length" =~ ^[0-9]*$ ]]; then
 		if [ "$length" -lt 12 ] || [ "$length" -gt 20 ]; then
 			echo "length must be between 12 and 20 characters"
@@ -16,3 +17,8 @@ while true; do
 		echo "invalid input"
 	fi
 done
+
+# generate string using CSPRNG
+password=$(head -c "$length" /dev/urandom | base64)
+
+echo "new password: $password"
