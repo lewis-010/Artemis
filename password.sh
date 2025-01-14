997d9273f9a8cbfdf59dcbb893687987f0cdb6c9
Lewis Cox <lcox101101@gmail.com>
2025-01-14 21:30:32 +0000

    minor improvements

--- password.sh
+++ password.sh
@@ -2,12 +2,11 @@
 
 # input length of new password
 while true; do
-	echo "length of new password:"
-	read length
+	read -p "length of new password: " length
 	# validate input
-	if [[ "$length" =~ ^[0-9]*$ ]]; then
-		if [ "$length" -lt 12 ] || [ "$length" -gt 20 ]; then
-			echo "length must be between 12 and 20 characters"
+	if [[ "$length" =~ ^[0-9]+$ ]]; then
+		if [ "$length" -lt 12 ] || [ "$length" -gt 25 ]; then
+			echo "length must be between 12 and 25 characters"
 		else
 			break
 		fi
